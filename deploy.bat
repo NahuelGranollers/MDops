540a8843f152222ea81861b717063ca621733e52
nahuel <nahuelgranollers@gmail.com>
2026-06-11 16:02:19 +0200

    chore: mejorado deploy.bat con pause y manejo de errores

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,45 @@
 @echo off
 setlocal enabledelayedexpansion
 
-if "%1"=="" (
+cd /d "%~dp0"
+
+if "%*"=="" (
   set msg=deploy: %DATE% %TIME%
 ) else (
   set msg=%*
 )
 
-echo ^> git add -A
-git add -A
-
-echo ^> git commit -m "%msg%"
-git commit -m "%msg%"
-
-echo ^> git push
-git push
-
+echo === MD Ops Deploy ===
 echo.
-echo Hecho. Esperando a GitHub Actions y Vercel...
+
+echo [1/4] git add -A
+git add -A
+if errorlevel 1 goto error
+
+echo [2/4] git commit -m "%msg%"
+git commit -m "%msg%"
+if errorlevel 1 (
+  if errorlevel 128 goto error
+  echo [!] Nada que commiter (no hay cambios nuevos^)
+  goto done
+)
+
+echo [3/4] git push
+git push
+if errorlevel 1 goto error
+
+echo [4/4] Despliegues iniciados.
+echo.
+echo === Seguimiento ===
 echo Frontend: https://github.com/NahuelGranollers/MDops/actions
 echo API:      https://vercel.com/nahuels-projects-b5553abb/m-dops-api
+echo URL web:  https://nahuelgranollers.github.io/MDops/
+goto done
+
+:error
+echo.
+echo [!] Error detectado. Revisa la salida arriba.
+
+:done
+echo.
+pause
